caaf5d2c58414c0b1859ac4270b79085d62985ce
Vladimir Yu. Ivanov <vladimir.u.ivanov@gmail.com>
2023-01-10 02:49:23 +1000

    Add decoupling client and back sides via queue (PoC)

--- main.cpp
+++ main.cpp
@@ -1,15 +1,32 @@
 #include <cassert>
 #include <cstdlib>
+#include <functional>
+#include <future>
 #include <iostream>
 #include <string>
+#include <utility>
+#include <variant>
 
 #include <cpr/cpr.h>
 #include <nlohmann/json.hpp>
+
+struct Result {
+    double a;
+};
 
 int main()
 {
     const char* const api_key = std::getenv("OPENAI_API_KEY");
     assert(api_key);
+
+    auto request_queue = std::queue<
+        std::pair<
+            cpr::AsyncResponse,
+            std::variant<
+                std::function<void(Result)>, std::promise<Result>
+            >
+        >
+    >{};
 
     auto async = cpr::PostAsync(
         cpr::Url{"https://api.openai.com/v1/completions"},
@@ -25,11 +42,24 @@ int main()
         }
     );
 
+    auto res = std::variant<
+        std::function<void(Result)>, std::promise<Result>>{};
+
+    res = std::promise<Result>{};
+    auto f = std::get<1>(res).get_future();
+    request_queue.emplace(std::pair{std::move(async), std::move(res)});
+
     std::cout << R"(Waiting ... )";
     std::cout.flush();
 
-    const auto response = async.get();
-    std::cout << response.elapsed << " sec" << '\n';
+    // Back side
+
+    auto [a, r] = std::move(request_queue.front());
+    request_queue.pop();
+
+    const auto response = a.get();
+    std::get<1>(r).set_value(Result{.a=response.elapsed});
+    std::cout << f.get().a << " sec" << '\n';
 
     assert(not response.error);
     assert(not response.text.empty());

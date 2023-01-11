c50cfe2047af2201cce144e19dd6922a80520426
Vladimir Yu. Ivanov <vladimir.u.ivanov@gmail.com>
2023-01-11 23:05:07 +1000

    Add base REST interface

--- main.cpp
+++ main.cpp
@@ -29,94 +29,149 @@ nlohmann::json to_json() const {                        \
     return j;                                           \
 }
 
-namespace Openai {
+namespace Openai::Impl {
 
 class RestInterface {
 public:
-    RestInterface()
-        : m_url{"https://api.openai.com/v1"}
-        , m_token{"OPENAI_API_KEY"}  // XXX: Token is not default constructible.
+    enum class Request {
+        POST,
+    };
+
+    struct Response {
+        uint16_t code;
+        std::string body;
+    };
+
+    virtual RestInterface& set_endpoint_path(const std::string& path) = 0;
+    virtual RestInterface& add_header_field(const std::string& name, const std::string& value) = 0;
+    virtual RestInterface& set_body_content(const std::string& data) = 0;
+
+    virtual std::future<Response> submit_request(Request type) = 0;
+
+    virtual ~RestInterface() = default;
+};
+
+class RestApi: public RestInterface {
+public:
+    RestApi(): m_token{"OPENAI_API_KEY"}  // XXX: Token is not default constructible.
     {
         const char* const api_key = std::getenv(m_token.GetToken());
         if (not api_key) {
             throw std::invalid_argument{m_token.GetToken() + std::string{" is not provided"}};
         }
         m_token = cpr::Bearer{api_key};
+        m_url = std::string{BASE_URL};
     }
-    void append_url(std::string&& append) {
-        m_url = m_url.str() + std::move(append);
+
+    RestInterface& set_endpoint_path(const std::string& path) override
+    {
+        m_url += path;
+        return (*this);
     }
-    void update_header(std::string&& key, std::string&& val) {
-        m_header.emplace(std::pair{std::move(key), std::move(val)});
+
+    RestInterface& add_header_field(const std::string& name, const std::string& value) override
+    {
+        m_header.emplace(std::pair{name, value});
+        return (*this);
     }
-    void set_body(std::string&& set) {
-        m_body = std::move(set);
+
+    RestInterface& set_body_content(const std::string& data) override
+    {
+        m_body = data;
+        return (*this);
     }
-    void make_post() {
-        m_request = cpr::PostAsync(m_url, m_token, m_header, m_body);
+
+    std::future<Response> submit_request(Request /*type*/) override
+    {
+        auto f_response = cpr::PostAsync(m_url, m_token, m_header, m_body);
+        auto p_response = std::promise<Response>{};
+
+        auto f_client = p_response.get_future();
+
+        cpr::async(
+            [](std::future<cpr::Response>& f_response, std::promise<Response>& p_response) {
+                auto cpr_response = f_response.get();
+                auto api_response = Response {
+                    .code = static_cast<decltype(Response::code)>(cpr_response.status_code),
+                    .body = std::move(cpr_response.text),
+                };
+                p_response.set_value(std::move(api_response));
+            },
+            std::move(f_response), std::move(p_response)
+        );
+
+        return f_client;
     }
-    cpr::Response get() {
-        return m_request.get();
-    }
+
 private:
+    constexpr static std::string_view BASE_URL{"https://api.openai.com/v1"};
+
     cpr::Url m_url;
     cpr::Bearer m_token;
     cpr::Header m_header;
     cpr::Body m_body;
-
-    cpr::AsyncResponse m_request;
 };
+
+}
+
+namespace Openai::Impl {
 
 class Completion {
 public:
-    Completion() {
-        rest.append_url("/completions");
-        rest.update_header("Content-Type", "application/json");
+    explicit Completion(std::unique_ptr<RestInterface>&& rest_api):
+        m_rest_api(std::move(rest_api))
+    {
+        m_rest_api.operator*()
+                  .set_endpoint_path("/completions")
+                  .add_header_field("Content-Type", "application/json");
     }
-    Completion& with_model(std::string_view idx) {
-        model = idx;
-        return (*this);
+
+    Completion& with_model(std::string_view idx)
+    {
+        model = idx; return (*this);
     }
-    Completion& with_prompt(std::string_view txt) {
-        prompt = txt;
-        return (*this);
+
+    Completion& with_prompt(std::string_view txt)
+    {
+        prompt = txt; return (*this);
     }
-    Completion&& create() {
-        rest.set_body(to_json().dump());
-        rest.make_post();
-        return std::move(*this);
+
+    std::future<RestInterface::Response> async()
+    {
+        return m_rest_api.operator*()
+                         .set_body_content(to_json().dump())
+                         .submit_request(RestInterface::Request::POST);
     }
-    cpr::Response get() {
-        return rest.get();
-    }
+
 private:
+    std::unique_ptr<RestInterface> m_rest_api;
+
     TO_JSON_METHOD(model, prompt);
 
     std::optional<std::string> model;
     std::optional<std::string> prompt;
-
-    RestInterface rest;
 };
 
 }
 
 int main()
 {
-    auto model = Openai::Completion{}
-        .with_model("text-davinci-003")
-        .with_prompt("I wanna to")
-        .create();
+    auto model = Openai::Impl::Completion{std::make_unique<Openai::Impl::RestApi>()}
+            .with_model("text-davinci-003")
+            .with_prompt("I wanna to")
+            .async();
 
     std::cout << R"(Waiting ... )";
     std::cout.flush();
 
     auto response = model.get();
+    std::cout << response.code << '\n';
 
-    std::cout << response.elapsed << " sec " << '\n';
-    std::cout << response.status_code << '\n';
-
-    assert(not response.error);
-    assert(not response.text.empty());
-
-    std::cout << '\n' << response.text << '\n';
+//    std::cout << response.elapsed << " sec " << '\n';
+//    std::cout << response.status_code << '\n';
+//
+//    assert(not response.error);
+//    assert(not response.text.empty());
+//
+    std::cout << '\n' << response.body << '\n';
 }

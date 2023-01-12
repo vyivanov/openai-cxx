05968b18e9ed577868667c1e2ad1e7f87271aa34
Vladimir Yu. Ivanov <vladimir.u.ivanov@gmail.com>
2023-01-13 01:46:29 +1000

    Add Result parser

--- main.cpp
+++ main.cpp
@@ -19,8 +19,8 @@ template<typename T>
 inline constexpr bool is_optional_v = is_optional<T>::value;
 
 #define OPTIONAL_TO_JSON(o)                 \
-static_assert(is_optional_v<decltype(o)>);  \
-if (o.has_value()) { j[#o] = (*o); }
+static_assert(is_optional_v<decltype(m_##o)>);  \
+if (m_##o.has_value()) { j[#o] = (*m_##o); }
 
 #define TO_JSON_METHOD(...)                             \
 nlohmann::json to_json() const {                        \
@@ -38,7 +38,7 @@ public:
     };
 
     struct Response {
-        uint16_t code;
+        std::uint16_t code;
         std::string body;
     };
 
@@ -51,9 +51,24 @@ public:
     virtual ~RestInterface() = default;
 };
 
-class RestApi: public RestInterface {
+std::future<RestInterface::Response> convert(std::future<cpr::Response> future)
+{
+    assert(future.valid());
+    return cpr::async(
+        [](auto& future) -> RestInterface::Response {
+            auto http_response = future.get();
+            return {
+                .code = static_cast<decltype(RestInterface::Response::code)>(http_response.status_code),
+                .body = std::move(http_response.text),
+            };
+        },
+        std::move(future)
+    );
+}
+
+class RestApiv1: public RestInterface {
 public:
-    RestApi(): m_token{"OPENAI_API_KEY"}  // XXX: Token is not default constructible.
+    RestApiv1(): m_token{"OPENAI_API_KEY"}  // XXX: Token is not default constructible.
     {
         const char* const api_key = std::getenv(m_token.GetToken());
         if (not api_key) {
@@ -81,26 +96,14 @@ public:
         return (*this);
     }
 
-    std::future<Response> submit_request(Request /*type*/) override
+    std::future<Response> submit_request(const Request type) override
     {
-        auto f_response = cpr::PostAsync(m_url, m_token, m_header, m_body);
-        auto p_response = std::promise<Response>{};
+        if (type == Request::POST)
+        {
+            return convert(cpr::PostAsync(m_url, m_token, m_header, m_body));
+        }
 
-        auto f_client = p_response.get_future();
-
-        cpr::async(
-            [](std::future<cpr::Response>& f_response, std::promise<Response>& p_response) {
-                auto cpr_response = f_response.get();
-                auto api_response = Response {
-                    .code = static_cast<decltype(Response::code)>(cpr_response.status_code),
-                    .body = std::move(cpr_response.text),
-                };
-                p_response.set_value(std::move(api_response));
-            },
-            std::move(f_response), std::move(p_response)
-        );
-
-        return f_client;
+        std::abort();
     }
 
 private:
@@ -118,54 +121,118 @@ namespace Openai::Impl {
 
 class Completion {
 public:
-    explicit Completion(std::unique_ptr<RestInterface>&& rest_api):
-        m_rest_api(std::move(rest_api))
+    explicit Completion(std::unique_ptr<RestInterface> rest_api): m_rest_api(std::move(rest_api))
     {
         m_rest_api.operator*()
                   .set_endpoint_path("/completions")
                   .add_header_field("Content-Type", "application/json");
     }
 
-    Completion& with_model(std::string_view idx)
+    void model(std::string_view idx)
     {
-        model = idx; return (*this);
+        m_model = idx;
     }
 
-    Completion& with_prompt(std::string_view txt)
+    void prompt(std::string_view txt)
     {
-        prompt = txt; return (*this);
+        m_prompt = txt;
     }
 
-    std::future<RestInterface::Response> async()
+    std::future<RestInterface::Response> submit()
     {
         return m_rest_api.operator*()
                          .set_body_content(to_json().dump())
                          .submit_request(RestInterface::Request::POST);
     }
 
+    class Result;
+
 private:
     std::unique_ptr<RestInterface> m_rest_api;
 
     TO_JSON_METHOD(model, prompt);
 
-    std::optional<std::string> model;
-    std::optional<std::string> prompt;
+    std::optional<std::string> m_model;
+    std::optional<std::string> m_prompt;
+};
+
+class Completion::Result {
+public:
+    struct Choice {
+        std::string text;
+    };
+    explicit Result(std::future<RestInterface::Response> response): m_response(std::move(response)) {
+
+    }
+    std::optional<std::time_t> created()
+    {
+        parse_response();
+        return m_created;
+    }
+    std::optional<Choice> choices() {
+        parse_response();
+        return m_choices;
+    }
+private:
+    void parse_response() {
+        if (m_response.valid()) {
+            auto out = m_response.get();
+            m_created = nlohmann::json::parse(out.body).value("created", 0);
+            m_choices = Choice{.text=nlohmann::json::parse(out.body).at("choices").at(0).value("text", "")};
+        }
+    }
+    std::future<RestInterface::Response> m_response;
+    std::optional<std::time_t> m_created;
+    std::optional<Choice> m_choices;
 };
 
 }
 
 int main()
 {
-    auto model = Openai::Impl::Completion{std::make_unique<Openai::Impl::RestApi>()}
-            .with_model("text-davinci-003")
-            .with_prompt("I wanna to")
-            .async();
+    auto test = nlohmann::json::parse(
+R"(
+{
+    "id": "cmpl-6XtQ30LBc9RimfVYTRd2wpselIszg",
+    "object": "text_completion",
+    "created": 1673535875,
+    "model": "text-davinci-003",
+    "choices":
+    [
+        {
+            "text": " go shopping\n\nSounds like fun! Where would you like to go shopping?",
+            "index": 0,
+            "logprobs": null,
+            "finish_reason": "length"
+        }
+    ],
+    "usage":
+    {
+        "prompt_tokens": 3,
+        "completion_tokens": 16,
+        "total_tokens": 19
+    }
+}
+)"
+    );
 
+//    int out{};
+//    test.at("created").get_to(out);
+//    std::cout << test << '\n';
+
+    auto completion = Openai::Impl::Completion{std::make_unique<Openai::Impl::RestApiv1>()};
+    completion.model("text-davinci-003");
+    completion.prompt("I wanna to");
+//
+    auto result = completion.submit();
+//
     std::cout << R"(Waiting ... )";
     std::cout.flush();
 
-    auto response = model.get();
-    std::cout << response.code << '\n';
+//    auto response = result.get();
+//    std::cout << response.code << '\n';
+
+    std::cout << Openai::Impl::Completion::Result{std::move(result)}.choices().value().text << '\n';
 
 //    std::cout << response.elapsed << " sec " << '\n';
 //    std::cout << response.status_code << '\n';
@@ -173,5 +240,5 @@ int main()
 //    assert(not response.error);
 //    assert(not response.text.empty());
 //
-    std::cout << '\n' << response.body << '\n';
+//    std::cout << '\n' << response.body << '\n';
 }

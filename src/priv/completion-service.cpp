fef7465e1fea56ffc9f20600b7981817e9ae1afb
Vladimir Yu. Ivanov <vladimir.u.ivanov@gmail.com>
2023-01-23 08:40:57 +1000

    Add Completion implementation

--- src/priv/completion-service.cpp
+++ src/priv/completion-service.cpp
@@ -1,52 +1,63 @@
 #include "completion-service.hpp"
 
+#include <cassert>
 #include <future>
 #include <memory>
 #include <string>
+#include <utility>
 #include <vector>
 
 #include "rest-connector.hpp"
+#include "ser-des.hpp"
 
 namespace Openai::Impl::Completion {
 
-Completion::Completion(std::unique_ptr<RestConnector> connector) noexcept
+Completion::Completion(std::unique_ptr<RestConnector> connector) noexcept:
+    m_connector(std::move(connector))
 {
-    (void) connector;
+    assert(m_connector);
+
+    m_connector->appendRoutePath(SERVICE_ROUTE_PATH);
+    m_connector->addHeaderField("content-type", "application/json");
 }
 
 void Completion::model(std::string&& id) noexcept
 {
-    (void) id;
+    m_model = std::move(id);
 }
 
 void Completion::prompt(std::string&& text) noexcept
 {
-    (void) text;
+    m_prompt = std::move(text);
 }
 
 void Completion::prompt(std::vector<std::string>&& text_list) noexcept
 {
-    (void) text_list;
+    m_prompt = std::move(text_list);
 }
 
 void Completion::temperature(float sampling) noexcept
 {
-    (void) sampling;
+    m_temperature = sampling;
 }
 
 void Completion::echo() noexcept
 {
-
+    m_echo = true;
 }
 
 auto Completion::submitRequest() -> std::future<RestConnector::Response>
 {
-    return decltype(submitRequest()){};
+    m_connector->setBodyContent(CALL_TO_JSON(Completion, this).dump());
+    return m_connector->submitPostRequest();
 }
 
-Result::Result(std::future<RestConnector::Response> response) noexcept
+DEFINE_TO_JSON(Completion, model, prompt, temperature, echo);
+
+Result::Result(std::future<RestConnector::Response> response) noexcept:
+    m_response(std::move(response))
 {
-    (void) response;
+    assert(m_response.valid());
 }
 
 }

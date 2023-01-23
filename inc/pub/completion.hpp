9041b7347c5a6c8cddac6d0d371a5bd5bcb3a9b3
Vladimir Yu. Ivanov <vladimir.u.ivanov@gmail.com>
2023-01-23 11:59:56 +1000

    Add Completion wrapper implementation

--- inc/pub/completion.hpp
+++ inc/pub/completion.hpp
@@ -1,13 +1,22 @@
 #pragma once
 
+#include <memory>
 #include <string>
 #include <vector>
 
 namespace Openai {
+namespace Impl::Completion { class Completion; class Result; }
 
 class Completion {
 public:
     Completion();
+    virtual ~Completion() noexcept;
+
+    Completion(const Completion&) = delete;
+    Completion& operator=(const Completion&) = delete;
+
+    Completion(Completion&&) noexcept;
+    Completion& operator=(Completion&&) noexcept;
 
     Completion& model(std::string id) noexcept;
 
@@ -20,10 +29,25 @@ public:
 
     class Result;
     Result create();
+
+private:
+    std::unique_ptr<Impl::Completion::Completion> m_pimpl;
 };
 
 class Completion::Result {
+friend class Completion;
+public:
+    Result();
+    virtual ~Result() noexcept;
 
+    Result(const Result&) = delete;
+    Result& operator=(const Result&) = delete;
+
+    Result(Result&&) noexcept;
+    Result& operator=(Result&&) noexcept;
+
+private:
+    std::unique_ptr<Impl::Completion::Result> m_pimpl;
 };
 
 }
--- src/pub/completion.cpp
+++ src/pub/completion.cpp
@@ -1,43 +1,65 @@
 #include "completion.hpp"
 
+#include <cassert>
 #include <string>
+#include <utility>
 #include <vector>
+
+#include "completion-service.hpp"
+#include "make.hpp"
 
 namespace Openai {
 
-Completion::Completion()
+Completion::Completion(): m_pimpl(Impl::MakeService<Impl::Completion::Completion>())
 {
-
+    assert(m_pimpl);
 }
 
 auto Completion::model(std::string id) noexcept -> Completion&
 {
-    (void) id; return (*this);
+    m_pimpl->model(std::move(id));
+
+    return (*this);
 }
 
 auto Completion::prompt(std::string text) noexcept -> Completion&
 {
-    (void) text; return (*this);
+    m_pimpl->prompt(std::move(text));
+
+    return (*this);
 }
 
 auto Completion::prompt(std::vector<std::string> text_list) noexcept -> Completion&
 {
-    (void) text_list; return (*this);
+    m_pimpl->prompt(std::move(text_list));
+
+    return (*this);
 }
 
 auto Completion::temperature(float sampling) noexcept -> Completion&
 {
-    (void) sampling; return (*this);
+    m_pimpl->temperature(sampling);
+
+    return (*this);
 }
 
 auto Completion::echo() noexcept -> Completion&
 {
+    m_pimpl->echo();
+
     return (*this);
 }
 
 auto Completion::create() -> Result
 {
-    return Completion::Result{};
+    auto result = Completion::Result{};
+    result.m_pimpl = Impl::MakeResult<Impl::Completion::Result>(m_pimpl);
+
+    assert(result.m_pimpl);
+    return result;
 }
+
+DEFINE_SPECIAL_MEMBERS_FOR_SERVICE(Completion);
+DEFINE_SPECIAL_MEMBERS_FOR_RESULT(Completion);
 
 }
